944d3a78656eb9173a85af06d4f877ae537935f5
Riley Moore <rilemoore@gmail.com>
2019-11-10 05:18:04 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -18,9 +18,8 @@ class Student
   def self.new_from_db(row)
     # create a new Student object given a row from the database
     
-    new_student = self.new(row[0],row[1],row[2])
+    new_student = self.new(row[1],row[2],row[0])
     new_student
-    binding.pry
   end
 
   def self.create_table
