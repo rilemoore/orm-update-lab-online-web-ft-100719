c01cfe368bf40ed0c9e8ebdf34d8fd6a309e2011
Riley Moore <rilemoore@gmail.com>
2019-11-10 05:21:08 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -56,6 +56,16 @@ class Student
     student
   end
 
+  def self.find_by_name(name)
+    # find the student in the database given a name
+    # return a new instance of the Student class
+    
+    sql = "SELECT * FROM students WHERE name = ?"
+    row = DB[:conn].execute(sql, name).flatten
+    self.new_from_db(row)
+    
+  end
+
 end
 
 
