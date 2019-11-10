795edd8d1cea88b43e8e6356efac11d1d29f07da
Riley Moore <rilemoore@gmail.com>
2019-11-10 05:22:24 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -40,7 +40,10 @@ class Student
   end
 
   def save
-    
+    if self.id
+      self.update
+    else
+      
     sql = <<-SQL
       INSERT INTO students (name, grade) 
       VALUES (?, ?)
@@ -48,6 +51,7 @@ class Student
 
     DB[:conn].execute(sql, self.name, self.grade)
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+    end
   end
 
   def self.create(name, grade)
