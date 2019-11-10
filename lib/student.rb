e0ed6b6acb194a5b854bb750021b04ea889c47c8
Riley Moore <rilemoore@gmail.com>
2019-11-10 05:03:22 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -15,6 +15,17 @@ class Student
     @id = id
   end
 
+  def self.new_from_db(row)
+    # create a new Student object given a row from the database
+    
+    new_student = self.new
+    new_student.id = row[0]
+    new_student.name = row[1]
+    new_student.grade = row[2]
+    new_student
+    
+  end
+
   def self.create_table
     sql = <<-SQL
     CREATE TABLE IF NOT EXISTS students (
