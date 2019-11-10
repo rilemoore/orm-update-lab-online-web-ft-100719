8a85b2a3254d050857ade17af13be208b67cb194
Riley Moore <rilemoore@gmail.com>
2019-11-10 04:50:45 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,7 +5,17 @@ class Student
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
 
-  def initialize()
+  attr_accessor :name, :grade
+  
+  attr_reader :id
+
+  def initialize(name, grade, id=nil)
+    @name = name
+    @grade = grade
+    @id = id
+  end
+
+  def self.create_table
     
   end
 
